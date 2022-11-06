9df876618986b750abfe9d24c12aba68499710b2
akatiwari <aruntiwari@outlook.in>
2022-11-06 23:25:02 +0530

    Assignment Masai

---
+++ Assignment 04112022/Q1.js
@@ -0,0 +1,19 @@
+function difference(a,b)
+{
+    return(a-b)
+}
+function abs(c)
+{
+    if(c<0)
+    {
+        return(-1*c)
+    }
+    else
+    {
+        return c
+
+    }
+
+}
+console.log(abs(difference(12,4)))
+console.log(abs(difference(4,18)))
